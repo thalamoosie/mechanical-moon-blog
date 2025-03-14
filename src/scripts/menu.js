f73a1dc613b5a969b032a21070398c70f30c9722
thalamoosie <thalamoose@protonmail.com>
2025-03-13 22:46:47 -0400

    Shuffle page contents, fix hamburger x

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,8 +1,11 @@
 const mobileMenu = document.querySelector(".mobile-nav");
 const hamburgerMenu = document.querySelector(".hamburger");
+const mobileNavMenu = document.getElementById("");
+const body = document.body;
 
 hamburgerMenu.addEventListener("click", () => {
   console.log("Ham menu clicked!");
   hamburgerMenu.classList.toggle("active");
   mobileMenu.classList.toggle("active");
+  body.classList.toggle("no-scroll");
 });
